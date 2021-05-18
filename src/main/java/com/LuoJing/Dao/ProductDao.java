0be7d4b8fd102a1d1e45585e1f141baec8861ada
LuoJing <1321024038@qq.com>
2021-05-18 19:36:23 +0800

    Week11

--- src/main/java/com/LuoJing/Dao/ProductDao.java
+++ src/main/java/com/LuoJing/Dao/ProductDao.java
@@ -5,7 +5,9 @@ import com.LuoJing.Model.*;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao implements  IProductDao{
@@ -32,43 +34,152 @@ public class ProductDao implements  IProductDao{
     }//end save
 
     @Override
-    public int delete(Integer productId, Connection con) {
-        return 0;
+    public int delete(Integer productId, Connection con) throws SQLException {
+        String sql = "delete from product where=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setInt(1,productId);
+        int count = stat.executeUpdate();
+        return count;
     }
 
     @Override
-    public int update(Product instance, Connection con) {
-        return 0;
+    public int update(Product instance, Connection con) throws SQLException {
+        String sql = "update product set ProductName=?,ProductDescription=?,Picture=?,Price=?,CategoryID=?where ProductId=?";
+
+        PreparedStatement stat = con.prepareStatement(sql);
+
+        stat.setString(1,instance.getProductName());
+        stat.setString(2,instance.getProductDescription());
+        stat.setString(3, String.valueOf(instance.getPicture()));
+        stat.setDouble(4,instance.getPrice());
+        stat.setInt(5,instance.getProductId());
+        stat.setInt(6,instance.getCategoryID());
+
+        int count = stat.executeUpdate();
+        return count;
     }
 
     @Override
-    public Product findById(Integer productId, Connection con) {
-        return null;
+    public Product findById(Integer productId, Connection con) throws SQLException {
+        String sql = "SELECT * from product where ProductId=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setInt(1,productId);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        if (rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+        }
+        return product;
     }
 
     @Override
-    public List<Product> findByCategoryId(int categoryId, Connection con) {
-        return null;
+    public List<Product> findByCategoryId(int categoryId, Connection con) throws SQLException {
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * from product where CategoryID=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setInt(1,categoryId);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        while (rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+            products.add(product);
+        }
+        return products;
     }
 
+//    ？？？？？
     @Override
     public List<Product> findByPrice(double minPrice, double maxPrice, Connection con) throws SQLException {
-        return null;
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * from product where Price=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setDouble(1,minPrice);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        while (rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+            products.add(product);
+        }
+        return products;
     }
 
     @Override
     public List<Product> findAll(Connection con) throws SQLException {
-
-        return null;
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * from product";
+        PreparedStatement stat = con.prepareStatement(sql);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        while(rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+            products.add(product);
+        }
+        return products;
     }
 
     @Override
     public List<Product> findByProductName(String productName, Connection con) throws SQLException {
-        return null;
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * from product where ProductName=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setString(1,productName);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        while (rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+            products.add(product);
+        }
+        return products;
     }
 
     @Override
     public List<Product> getPicture(Integer productId, Connection con) throws SQLException {
-        return null;
+        List<Product> products = new ArrayList<>();
+        String sql = "SELECT * from product where ProductId=?";
+        PreparedStatement stat = con.prepareStatement(sql);
+        stat.setInt(1,productId);
+        ResultSet rs = stat.executeQuery();
+        Product product = null ;
+        while (rs.next()){
+            product = new Product();
+            product.setProductId(rs.getInt("ProductId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductDescription(rs.getString("ProductDescription"));
+//            product.setPicture(rs.getString("Picture"));
+            product.setPrice(rs.getDouble("Price"));
+            product.setCategoryID(rs.getInt("CategoryID"));
+            products.add(product);
+        }
+        return products;
     }
 }
