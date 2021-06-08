1c6d2d0ee84685d3ad6278b4b446651cb0994cb0
LuoJing <1321024038@qq.com>
2021-06-08 23:26:00 +0800

    lab2

--- src/main/java/com/lab2/LuoJingFilter.java
+++ src/main/java/com/lab2/LuoJingFilter.java
@@ -5,15 +5,15 @@ import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 
-@WebFilter(filterName = "TyyFilter")
+@WebFilter(filterName = "LuoJingFilter")
 public class LuoJingFilter implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        System.out.println("TyyFilter -- before chain");
+        System.out.println("LuoJingFilter -- before chain");
         chain.doFilter(req, resp);
-        System.out.println("TyyFilter -- after chain");
+        System.out.println("LuoJingFilter -- after chain");
     }
 
     public void init(FilterConfig config) throws ServletException {
